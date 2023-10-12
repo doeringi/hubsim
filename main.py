b7625249031fdc44eae0c753b09ad163dd27a5df
doeringi <doeringi@hs-pforzheim.de>
2023-10-12 17:27:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ llm.load_llm()
 
 t5_small_local_llm = llm.get_llm
 
-prompt = "You are John. Sara your wife asks what you want to eat for dinner. Your answer to Saras question:"
+prompt = "You are Peter. Sara your wife asks what you want to eat for dinner. Your answer to Saras question:"
 
 output = t5_small_local_llm(prompt)
 
