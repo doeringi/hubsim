9ed833bd4d74c2f87b0401ade94a495880b75f4a
doeringi <doeringi@hs-pforzheim.de>
2023-11-09 21:44:03 +0100

    Add single factor variants function

--- components/common/variant_testing_helper.py
+++ components/common/variant_testing_helper.py
@@ -95,6 +95,52 @@ def all_variants():
     return [(variant) for variant in all_variants]
 
 
+def single_factor_variants():
+    apartment = _load_product_config()
+    # landlord_renter = landlord_renter_combination()
+
+    landlord_name, landlord_socio_behavioral, landlord_target = _load_landlord_config()
+    renter_name, renter_socio_behavioral, renter_target = _load_renter_config()
+
+    landlord_renter = [
+        (
+            {
+                "name_id": landlord_name[0]["id"],
+                "socio_behavioral_id": landlord_socio_behavioral[0]["id"],
+                "target_id": landlord_target[0]["id"],
+            },
+            {
+                "name_id": renter_name[0]["id"],
+                "socio_behavioral_id": renter_socio_behavioral[0]["id"],
+                "target_id": renter_target[0]["id"],
+            },
+        )
+    ]
+
+    factor_variants = product(landlord_renter, apartment)
+
+    return [
+        (
+            variant,
+            {
+                "landlord_system_message": render_system_message(
+                    landlord_name[0]["content"],
+                    landlord_socio_behavioral[0]["content"],
+                    landlord_target[0]["content"],
+                    variant[1]["content"],
+                ),
+                "renter_system_message": render_system_message(
+                    renter_name[0]["content"],
+                    renter_socio_behavioral[0]["content"],
+                    renter_target[0]["content"],
+                    variant[1]["content"],
+                ),
+            },
+        )
+        for variant in factor_variants
+    ]
+
+
 def render_system_message(
     name: str, socio_behavioral: str, target: str, apartment: str
 ) -> str:
@@ -109,8 +155,8 @@ def render_system_message(
 
 
 # example all variants
-all = all_variants()
-print(len(all))  # get number of all variants
-# print(all[0])  # get first variant of landlord and renter variant combination
-# print(all[0][0])  # get first variant of landlord variants
-print(all[0][0][1])  # get system message of first variant of landlord variants
+all = single_factor_variants()
+print(len(all))
+print(all)
+# print(all[0][0])
+# print(all[0][0][1])
