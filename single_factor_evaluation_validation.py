af39ce0aa8dfb456d48e4ab17ba49c59d2c5f2ea
Jens Rupprecht <jrupprec@mail.uni-mannheim.de>
2024-02-22 15:00:00 +0100

    interview results analysis

--- single_factor_evaluation_validation.py
+++ single_factor_evaluation_validation.py
@@ -56,7 +56,7 @@ bagel_llm_config = {
 experiment_helper = BaseExperiment()
 now = datetime.now() # current date and time
 base_path = "single-factor-experiments"
-experiment_path = "Yi-34B-Chat-Yi-34B-Chat"
+experiment_path = "bagel-dpo-34b-v0.2-bagel-dpo-34b-v0.2"
 evaluation_folder = "single-factor-controlled-evaluation-results-validation-" + now.strftime("%m%d%Y")
 
 full_path = os.path.join(base_path, experiment_path)
