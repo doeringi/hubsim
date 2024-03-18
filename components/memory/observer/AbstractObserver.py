a8bede20888502c90113ac6dcb7330e96d7ec0ce
doeringi <doeringi@hs-pforzheim.de>
2024-03-18 21:05:59 +0100

    repo clean-up

--- components/memory/observer/AbstractObserver.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractObserver(ABC):
-    @abstractmethod
-    def observe(self) -> str:
-        raise NotImplementedError('Abstract method "observe" must be implemented')
--- components/world/World.py
+++
@@ -1,3 +0,0 @@
-# this is the world class
-# it contains all the objects in the world
-# it also contains all agents in the world
